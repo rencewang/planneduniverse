b4e333bf3f450de0b14b673a795b3eb822264f7c
law-wang <47406212+law-wang@users.noreply.github.com>
2020-07-13 13:03:22 -0700

    theme

---
+++ .cache/sync-requires.js
@@ -0,0 +1,15 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Volumes/Data/Git/fpworld/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Volumes/Data/Git/fpworld/src/pages/404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Volumes/Data/Git/fpworld/src/pages/about.js"))),
+  "component---src-pages-elements-js": hot(preferDefault(require("/Volumes/Data/Git/fpworld/src/pages/elements.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Volumes/Data/Git/fpworld/src/pages/index.js"))),
+  "component---src-templates-blog-post-js": hot(preferDefault(require("/Volumes/Data/Git/fpworld/src/templates/blog-post.js")))
+}
+
---
+++ src/pages/404.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+class NotFoundPage extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="Not Found" />
+        <h1>Page Not Found</h1>
+        <p>Try a different route</p>
+      </Layout>
+    )
+  }
+}
+
+export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
